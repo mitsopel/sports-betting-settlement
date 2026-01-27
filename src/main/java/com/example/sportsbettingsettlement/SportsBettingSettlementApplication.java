1d225413f2dfe3d523c2702bd8296e9f23b1b4df
emitsopoulou <emitsopoulou@trading-point.com>
2026-01-27 21:21:12 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/example/sportsbettingsettlement/SportsBettingSettlementApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportsbettingsettlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsBettingSettlementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsBettingSettlementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sportsbettingsettlement/SportsBettingSettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportsbettingsettlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsBettingSettlementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
