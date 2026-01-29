a0d1d30bcf21e7b4539533b434232798987d093b
emitsopoulou <emitsopoulou@trading-point.com>
2026-01-29 21:33:35 +0200

    refactor DataInitializer hardcoded values to static finals

--- src/main/java/com/example/sportsbettingsettlement/seed/DataInitializer.java
+++ src/main/java/com/example/sportsbettingsettlement/seed/DataInitializer.java
@@ -12,12 +12,34 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
+    private static final long BET_ID_1 = 1L;
+    private static final long BET_ID_2 = 2L;
+    private static final long BET_ID_3 = 3L;
+
+    private static final long USER_ID_101 = 101L;
+    private static final long USER_ID_102 = 102L;
+    private static final long USER_ID_103 = 103L;
+
+    private static final String EVENT_ID_1 = "EVT-1";
+    private static final String EVENT_ID_2 = "EVT-2";
+
+    private static final String MARKET_ID_1 = "MKT-1";
+    private static final String MARKET_ID_2 = "MKT-2";
+
+    private static final String WINNER_ID_1 = "WIN-1";
+    private static final String WINNER_ID_2 = "WIN-2";
+    private static final String WINNER_ID_3 = "WIN-3";
+
+    private static final BigDecimal BET_AMOUNT_10 = BigDecimal.valueOf(10);
+    private static final BigDecimal BET_AMOUNT_20 = BigDecimal.valueOf(20);
+    private static final BigDecimal BET_AMOUNT_15 = BigDecimal.valueOf(15);
+
     private final BetRepository betRepository;
 
     @Override
     public void run(String... args) {
-        betRepository.save(new BetEntity(1L, 101L, "EVT-1", "MKT-1", "WIN-1", BigDecimal.valueOf(10)));
-        betRepository.save(new BetEntity(2L, 102L, "EVT-1", "MKT-1", "WIN-2", BigDecimal.valueOf(20)));
-        betRepository.save(new BetEntity(3L, 103L, "EVT-2", "MKT-2", "WIN-3", BigDecimal.valueOf(15)));
+        betRepository.save(new BetEntity(BET_ID_1, USER_ID_101, EVENT_ID_1, MARKET_ID_1, WINNER_ID_1, BET_AMOUNT_10));
+        betRepository.save(new BetEntity(BET_ID_2, USER_ID_102, EVENT_ID_1, MARKET_ID_1, WINNER_ID_2, BET_AMOUNT_20));
+        betRepository.save(new BetEntity(BET_ID_3, USER_ID_103, EVENT_ID_2, MARKET_ID_2, WINNER_ID_3, BET_AMOUNT_15));
     }
 }
